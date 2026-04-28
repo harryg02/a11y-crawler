8f4571ab5102d19169ee69b9eeb25e97fcef7b67
harryg02 <94257270+harryg02@users.noreply.github.com>
2026-04-28 11:26:34 -0400

    update styles on History.tsx

--- app/components/History.tsx
+++ app/components/History.tsx
@@ -2,7 +2,7 @@
 
 export default function History() {
   return (
-    <div>
+    <div className="p-8">
       <h2 className="text-3xl font-medium mb-6">History</h2>
       {/* past crawls list */}
     </div>
