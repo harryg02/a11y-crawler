e86c196475555543fed55200498a633ae3101a95
harryg02 <94257270+harryg02@users.noreply.github.com>
2026-05-16 14:24:31 -0400

    added styling change to settings page

--- app/components/Settings.tsx
+++ app/components/Settings.tsx
@@ -2,7 +2,7 @@
 
 export default function Settings() {
   return (
-    <div>
+    <div className="max-w-220 mx-auto p-8">
       <h2 className="text-3xl font-medium mb-6">Settings</h2>
       {/* app-wide settings */}
     </div>
